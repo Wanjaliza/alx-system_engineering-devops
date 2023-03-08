ba9d4832246ab9c508ce3b1cc3116bc5f5465818
iankisali <iankisali@gmail.com>
2023-03-08 17:15:28 +0000

    increasing hard&soft user login into holberton

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,13 @@
 # Changing OS configuration to enable login with holberton
 
 # Increasing hard file limit 
-exec { 'increasing-hard-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
-# Increasing soft file limit
-exec { 'increasing-soft-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000" /etc/security/limits.conf'
+# Increase soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
-  }
+}
